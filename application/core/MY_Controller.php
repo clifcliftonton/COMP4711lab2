1151a349393b2df34833f57ea9953ec446c27f85
Clifton Tam <ctam@live.ca>
2016-01-26 15:40:38 -0800

    #ci-basic02 partially complete

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * core/MY_Controller.php
+ * 
+ * Default application controller
+ */
+class Application extends CI_Controller {
+    protected $data = array();  // parameters for view components
+    protected $id;
+    protected $choices = array(
+        'Home' => '/',
+        'Gallery' => '/gallery', 
+        'About' => '/about'
+    ); //menu navbar
+    
+    /**
+     * Constructor 
+     * Establish view parameters 
+     */
+    
+    function _construct() {
+        parent::_construct();
+        $this->data = array();
+        $this->data['pagetitle'] = 'Sample Image Gallery';
+    }
+    
+    /**
+     * Render this page
+     * 
+     */
+    function render() {
+        $this->data['menubar'] = build_menu_bar($this->choices);
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+        $this->data['data'] = &$this->data;
+        $this->parser->parse('_template', $this->data);
+    }
+}
+/* End of MY_Controller.php */
+/* Location: application/core/MY_Controller.php */
